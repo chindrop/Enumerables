c00c58b877e1f1417a340a4d2bb3af5e26b5ef6e
chindrop <apronticaleb@gmail.com>
2020-06-09 23:46:08 +0000

    Added any methods

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Enumerable
   def my_each
     if block_given?
@@ -9,6 +11,72 @@ module Enumerable
       to_enum(:my_each)
     end
   end
-  a = ['a', 'b', 'c']
-  a.my_each {|x| puts x, "--"}
+
+  def my_each_with_index
+    if block_given?
+      length.times do |elem|
+        yield self[elem], index
+      end
+      self
+    else
+      to_enum(:my_each_with_index)
+    end
+  end
+
+  def my_select
+    result = []
+    if block_given?
+      my_each do |element|
+        result << element if yield element
+      end
+      result
+    else
+      to_enum(:my_select)
+    end
+  end
+  # rubocop:disable Style/CaseEquality
+
+  def my_all?(arg = nil)
+    my_each do |element|
+      if block_given?
+        return false unless yield element
+      elsif arg.nil?
+        return false unless element
+      else
+        return false unless arg === element
+      end
+    end
+    true
+  end
+
+  def my_any?(arg = nil)
+    my_each do |element|
+      if block_given?
+        return true unless yield element
+      elsif arg.nil?
+        return true unless element
+      else
+        return true unless arg === element
+      end
+    end
+    false
+  end
+
+  def my_none?(arg = nil)
+    my_each do |element|
+      if block_given?
+        return false unless yield element
+      elsif arg.nil?
+        return false unless element
+      else
+        return false unless arg === element
+      end
+    end
+    false
+  end
+  # rubocop:enable Style/CaseEquality
 end
+
+fruits = ["apple", "banana", "strawberry", "pineapple"]
+
+puts fruits.my_none? { |fruit| fruit.length > 10 }
